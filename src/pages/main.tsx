7cedffbe30800d95425ad9b1677d8822638ff4e5
Matodev11 <msmojvir1@gmail.com>
2024-04-05 18:36:30 +0200

    Added top navigation, scss, and login window

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+import IconLogo from "../assets/logo";
+import SearchIcon from "../assets/search";
+import IconShuffle from "../assets/shuffle";
+
+type navMainType = {
+  mainLink: string;
+}
+
+
+const navMain: navMainType[] = [
+
+{mainLink:"Feature"},
+{mainLink:"Discover"},
+{mainLink:"Shows"},
+{mainLink:"Fans"},
+{mainLink:"Pricing"},
+{mainLink:"Boiler room"},
+{mainLink:"Videos"},
+{mainLink:"Help center"},
+
+]
+
 type artistDataType = {
   Artistname: string;
   Artistfollowers: number;
@@ -37,52 +60,129 @@ const songData: songDataType[] = [
   {
     artistImg: "https://source.unsplash.com/random/?music-artist1",
     artistSongName: "Beirut",
-    artistAlbumName: "wqjrfia",
+    artistAlbumName: "Serenade of Shadows",
     artistTime: 11,
     artistPlays: 500.431,
   },
   {
     artistImg: "https://source.unsplash.com/random/?music-artist2",
     artistSongName: "Minerta",
-    artistAlbumName: "qwgagae",
+    artistAlbumName: "Whispers in the Mist",
     artistTime: 6,
     artistPlays: 200.332,
   },
   {
     artistImg: "https://source.unsplash.com/random/?music-artist3",
-    artistSongName: "Huhueh",
-    artistAlbumName: "lawjgfoiqawhj",
+    artistSongName: "Bloodlust Anthem",
+    artistAlbumName: "Brutal Revelation",
     artistTime: 2,
     artistPlays: 200.031,
   },
   {
     artistImg: "https://source.unsplash.com/random/?music-artist4",
-    artistSongName: "hwhehe",
-    artistAlbumName: "qwgagae",
+    artistSongName: "Hellfire Symphony",
+    artistAlbumName: "Chaos Reigns",
     artistTime: 13,
     artistPlays: 120.431,
   },
   {
     artistImg: "https://source.unsplash.com/random/?music-artist5",
-    artistSongName: "qwasvgrfsn",
-    artistAlbumName: "qwgagae",
+    artistSongName: "Sinister Slaps",
+    artistAlbumName: "Graveyard Groove",
     artistTime: 7,
     artistPlays: 600.152,
   },
   {
     artistImg: "https://source.unsplash.com/random/?music-artist6",
-    artistSongName: "gqwtg43z43",
-    artistAlbumName: "qwgagae",
+    artistSongName: "Vibin' in the Void",
+    artistAlbumName: "Murderous Melodies",
     artistTime: 10,
     artistPlays: 100.022,
   },
 ];
 
+type albumDataType = {
+  albumImg: string;
+  albumName:string;
+}
+
+
+const albumData: albumDataType[] = [
+
+{albumImg: "https://source.unsplash.com/random/?music-album1", albumName:"Eternal Echoes"},
+{albumImg: "https://source.unsplash.com/random/?music-album2", albumName:"Midnight Melodies"},
+{albumImg: "https://source.unsplash.com/random/?music-album3", albumName:"Whispers in the Wind"},
+{albumImg: "https://source.unsplash.com/random/?music-album4", albumName:"Luminescent Dreams"},
+{albumImg: "https://source.unsplash.com/random/?music-album5", albumName:"Infinite Horizons"},
+{albumImg: "https://source.unsplash.com/random/?music-album6", albumName:"Sonic Reverie"}
+
+
+]
+
+type loginDataType = {
+  input1:string;
+placeholder1:string;
+input2:string;
+placheolder2:string;}
+
+
+
+const loginData:loginDataType = {
+
+input1:"text",
+placeholder1:"username",
+input2:"password",
+placheolder2:"password",
+
+}
+
+
 const Main = () => {
+
+const [login, setLogin] = useState<loginDataType | null>(null)
+
+const showLogin = () => {
+setLogin(loginData)
+}
+
+
   return (
     <>
+
       <div className="wrap">
+      {login && (
+
+<div className="loginbox">
+<h2 className="loginbox__h2">Login info</h2>
+<div className="loginbox__close">
+<div onClick={()=> setLogin(null)} className="loginbox__close--2">X</div>
+</div>
+<input className="loginbox__inp1" type={loginData.input1} placeholder={loginData.placeholder1} />
+<input className="loginbox__inp2" type={loginData.input2} placeholder={loginData.placheolder2} />
+</div>
+
+)}
         <div className="profiletop">
+        
+          <div className="profiletop__navtop">
+            <div className="profiletop__navtop--flex1">
+          <IconLogo/>
+        <input className="profiletop__input" placeholder="Search" type="text" /> 
+        <SearchIcon/> 
+        <nav className="profiletop__navtop--nav">  
+        {navMain.map((a)=> {
+return <a href={a.mainLink}>{a.mainLink}</a>
+})} 
+</nav>
+</div>
+
+<div className="profiletop__navtop--flex2">
+<button onClick={showLogin} className="profiletop__ghost">LOGIN</button>
+<button className="profiletop__active">SIGN UP FOR FREE</button>
+
+</div>
+
+          </div>
           <img
             className="profiletop__img"
             src={artistData.Artistimg}
@@ -102,7 +202,19 @@ const Main = () => {
           </nav>
         </div>
         <main className="profileinfo">
+        
+
+
           <div className="profileinfo__music">
+            <div className="profileinfo__flex">
+          <h2 className="profileinfo__h2">Featured songs</h2>
+          <div className="profileinfo__shuffle">
+          <p className="profileinfo__shuffle--p">SHUFFLE </p>
+          <IconShuffle/>
+          </div>
+          
+          
+          </div>
             {songData.map((song) => {
               return (
                 <div className="profileinfo__songtab">
@@ -115,7 +227,19 @@ const Main = () => {
               );
             })}
           </div>
+          <div className="album">
+{albumData.map((album)=> {
+return (
+  <div className="album__div">
+<img className="album__img" src={album.albumImg} alt={album.albumName}/>
+<p className="album__p">{album.albumName}</p>
+</div>
+)
+})}
+
+          </div>
         </main>
+       
       </div>
     </>
   );
