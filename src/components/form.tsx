5fe84c6b2b5d7c6801bb55c1a50670d14e3fa354
Mato <msmojvir1@gmail.com>
2024-04-23 20:13:53 +0200

    added time react picker

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import DatePicker from "react-date-picker";
+import TimePicker from "react-time-picker";
 
 type ValuePiece = Date | null;
 
@@ -7,6 +8,11 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const Form = () => {
   const [selectedDate, setSelectedDate] = useState<Value>(new Date());
+  const [selectedTime, setSelectedTime] = useState<string | null>("10:00");
+
+  const handleTimeChange = (time: string | null) => {
+    setSelectedTime(time);
+  };
 
   const handleDateChange = (date: Value) => {
     setSelectedDate(date);
@@ -15,13 +21,16 @@ const Form = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Selected date:", selectedDate);
+    console.log("Selected time:", selectedTime);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="field">
-        <label htmlFor="date">Select date</label> <br />
         <DatePicker value={selectedDate} onChange={handleDateChange} />
+      </div>
+      <div className="field">
+        <TimePicker onChange={handleTimeChange} value={selectedTime} />
       </div>
       <button className="formbtn" style={{ width: "10rem" }} type="submit">
         Submit
